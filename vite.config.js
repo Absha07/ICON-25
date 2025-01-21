488c3d7c77e549ab8e0d11a4a53df580a65fbd8b
Absha07 <abshaabdulla007@gmail.com>
2025-01-21 15:20:45 +0530

    Initial commit

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,10 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/ICON-25/src/App.jsx"
+  base: "/ICON-25/",
+  build: {
+    rollupOptions: {
+      input: '/index.html',
+    },
+  },
 })
