d9aec606e6ca7a22e0c755c52f47b005d79cde28
Absha07 <abshaabdulla007@gmail.com>
2025-01-21 15:34:15 +0530

    Initial commit after renaming and editing

--- src/App.jsx
+++ src/App.jsx
@@ -61,7 +61,69 @@
 
 
 
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
+// import { BrowserRouter, Route, Routes } from 'react-router-dom'
+
+// // 1
+// // import './App.css';
+// import './styles/styles.css'
+
+// import Particle from './components/Particle'
+
+// import Home from './pages/Home'
+// import Registration from './pages/Registration'
+// import Navbar from './components/Navbar'
+// import Mindscape from './pages/tech/CodeRoulette'
+// import CodeRoulette from './pages/tech/CodeRoulette';
+// import GreyScale from './pages/tech/GreyScale'
+// import TechQuest from './pages/tech/TechQuest'
+
+// import GameVerse from './pages/nontech/GameVerse'
+// import FilmFrency from './pages/nontech/FilmFrency'
+// import ApexConquest from './pages/nontech/ApexConquest'
+// import BioScope from './pages/nontech/BioScope'
+
+// import Footer from './components/Footer'
+
+// //2
+// import Update from './components/Update'
+
+
+
+
+// function App() {
+//   return (
+//     <BrowserRouter>
+      
+//       <Particle />
+//       <Navbar />
+//       <Routes>
+//         <Route path="/" element={<Home />} />
+//         <Route path="/registration" element={<Registration />} /> 
+
+//         {/* Technical Events */}
+//         <Route path='/event/mindscapes-unfold' element={<Mindscape/>} />
+//         <Route path='/event/CodeRoulette' element={<CodeRoulette/>} />
+//         <Route path='/event/greyscale' element={<GreyScale/>} />
+//         <Route path='/event/TechQuest' element={<TechQuest/>} />
+
+//         {/* Non-Technical Events */}
+//         <Route path='/event/snap-seek' element={<GameVerse/>} />
+//         <Route path='/event/film-frency' element={<FilmFrency/>} />
+//         <Route path='/event/apex-conquest' element={<ApexConquest/>} />
+//         <Route path='/event/BioScope' element={<BioScope/>} />
+
+//         {/* Test Route - temp */}
+//         {/* <Route path='/event/update' element={<Update/>} /> */}
+//       </Routes>
+//       <Footer/>
+//     </BrowserRouter>
+//   );
+// }
+
+// export default App;
+
+
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
 // 1
 // import './App.css';
@@ -88,36 +150,28 @@ import Footer from './components/Footer'
 import Update from './components/Update'
 
 
-
-
 function App() {
   return (
-    <BrowserRouter>
-      
+    <Router basename="/ICON-25">
       <Particle />
       <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/registration" element={<Registration />} /> 
-
+        <Route path="/registration" element={<Registration />} />
         {/* Technical Events */}
-        <Route path='/event/mindscapes-unfold' element={<Mindscape/>} />
-        <Route path='/event/CodeRoulette' element={<CodeRoulette/>} />
-        <Route path='/event/greyscale' element={<GreyScale/>} />
-        <Route path='/event/TechQuest' element={<TechQuest/>} />
-
+        <Route path='/event/mindscapes-unfold' element={<Mindscape />} />
+        <Route path='/event/CodeRoulette' element={<CodeRoulette />} />
+        <Route path='/event/greyscale' element={<GreyScale />} />
+        <Route path='/event/TechQuest' element={<TechQuest />} />
         {/* Non-Technical Events */}
-        <Route path='/event/snap-seek' element={<GameVerse/>} />
-        <Route path='/event/film-frency' element={<FilmFrency/>} />
-        <Route path='/event/apex-conquest' element={<ApexConquest/>} />
-        <Route path='/event/BioScope' element={<BioScope/>} />
-
+        <Route path='/event/snap-seek' element={<GameVerse />} />
+        <Route path='/event/film-frency' element={<FilmFrency />} />
+        <Route path='/event/apex-conquest' element={<ApexConquest />} />
+        <Route path='/event/BioScope' element={<BioScope />} />
         {/* Test Route - temp */}
-        {/* <Route path='/event/update' element={<Update/>} /> */}
+        {/* <Route path='/event/update' element={<Update />} /> */}
       </Routes>
-      <Footer/>
-    </BrowserRouter>
+      <Footer />
+    </Router>
   );
 }
-
-export default App;
--- vite.config.js
+++ vite.config.js
@@ -4,10 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/ICON-25/",
-  build: {
-    rollupOptions: {
-      input: '/index.html',
-    },
-  },
-})
+  base: "/ICON-25/"
+    })
